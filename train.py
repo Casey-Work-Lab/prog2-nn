8ca296450f73173bca58d1742a419fecde1060db
Keishi Tooi <j3354mfg@sit.ac.jp>
2024-12-10 16:31:08 +0900

    時間の表示を全てにおいて行う

--- train.py
+++ train.py
@@ -77,17 +77,23 @@ for k in range(n_epochs):
     loss_train_history.append(loss_train)
     print(f"train loss: {loss_train:.3f} ({time_end - time_start:.1f}s)", end=", ")
 
+    time_start = time.time()
     loss_test = models.test(model, dataloader_test, loss_fn)
     loss_test_history.append(loss_test)
-    print(f"test loss: {loss_test:.3f}", end=", ")
+    time_end = time.time()
+    print(f"test loss: {loss_test:.3f} ({time_end - time_start:.1f}s)", end=", ")
 
+    time_start = time.time()
     acc_train = models.test_accuracy(model, dataloader_train)
     acc_train_history.append(acc_train)
-    print(f"train accuracy:{acc_train*100:.2f}%", end=", ")
+    time_end = time.time()
+    print(f"train accuracy:{acc_train*100:.2f}% ({time_end - time_start:.1f}s)", end=", ")
 
+    time_start = time.time()
     acc_test = models.test_accuracy(model, dataloader_test)
     acc_test_history.append(acc_test)
-    print(f"test accuracy:{acc_test*100:.2f}%")
+    time_end = time.time()
+    print(f"test accuracy:{acc_test*100:.2f}% ({time_end - time_start:.1f}s)")
 
 plt.plot(acc_train_history, label="train")
 plt.plot(acc_test_history, label="test")
